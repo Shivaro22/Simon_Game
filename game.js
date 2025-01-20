be5b0578c9ab0811c74faabc839558b4f77d84e6
shiv.arora. <shiv.arora@deeplogictech.com>
2025-01-20 13:47:51 +0530

    Added playSound function

--- game.js
+++ game.js
@@ -1,12 +1,40 @@
 const buttonColors = ['red',"blue","green","yellow"];
 const gamePattern=[];
+const userClickedPattern = [];
+var randomColor;
+
 function nextSequence(){
     var randomNumber = (Math.random()*3).toFixed(0) ;
     console.log(randomNumber);
     var randomChosenColor = buttonColors[randomNumber];
+    randomColor = randomChosenColor;
     console.log(randomChosenColor);
-    gamePattern.add(randomChosenColor);
+    gamePattern.push(randomChosenColor);
+    $('#' + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
+    playSound(randomChosenColor);
+
+
+    $(".btn").click(function(){
+        var userChosenColor = $(this).attr('id');
+        
+        userClickedPattern.push(userChosenColor);
+        playSound(userChosenColor);
+        console.log(userClickedPattern);
+    });
+
 }
+
+function playSound(name){
+    $('#' + name).click(function() {
+        var audio = new Audio('/sounds/' + name + '.mp3');  // Dynamically select the sound based on color
+        audio.play().catch(function(error) {
+            console.error('Audio play error:', error);
+        });
+    });
+}
+
 nextSequence();
 
 //console.log(randomChosenColor);
