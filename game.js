da822ce6b12ba5676e42134d7a5684faf813e9a7
shiv.arora. <shiv.arora@deeplogictech.com>
2025-01-20 15:21:34 +0530

    Adde shadows

--- game.js
+++ game.js
@@ -2,27 +2,30 @@ const buttonColors = ['red',"blue","green","yellow"];
 const gamePattern=[];
 const userClickedPattern = [];
 var randomColor;
+var startOrNo = false;
+var level =0;
+$("h1").text("Press A key to start");
+$(document).keypress(function(){
+    if(startOrNo===false){
+        startOrNo=true;
+        nextSequence();
+    }
+})
+
 
 function nextSequence(){
-    var randomNumber = (Math.random()*3).toFixed(0) ;
-    console.log(randomNumber);
+    var randomNumber = Math.floor(Math.random() * 4)
+    
     var randomChosenColor = buttonColors[randomNumber];
     randomColor = randomChosenColor;
+    console.log(randomNumber);
     console.log(randomChosenColor);
     gamePattern.push(randomChosenColor);
     $('#' + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
-
+    level++;
+    $("h1").text("Level "+level);
+    console.log(level);
     playSound(randomChosenColor);
-
-
-    $(".btn").click(function(){
-        var userChosenColor = $(this).attr('id');
-        
-        userClickedPattern.push(userChosenColor);
-        playSound(userChosenColor);
-        console.log(userClickedPattern);
-    });
 
 }
 
@@ -32,9 +35,25 @@ function playSound(name){
         audio.play().catch(function(error) {
             console.error('Audio play error:', error);
         });
+        animatePress(name);
     });
 }
 
-nextSequence();
+function animatePress(currentColour){
+   $('#' + currentColour).addClass("pressed"),1000;
+   setTimeout(function(){
+    $("#"+currentColour).removeClass("pressed");
+   },100)
+}
+
+$(".btn").click(function(){
+    var userChosenColor = $(this).attr('id');
+    
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    console.log(userClickedPattern);
+});
+
+
 
 //console.log(randomChosenColor);
